b10f4dff37f1b2b833d93029b1374d1e724e43f2
ziy <1936893077@qq.com>
2025-11-21 17:39:12 +0800

    init commit

---
+++ src/main/java/me/ziyframework/agent/palantir/java/format/PalantirJavaFormatFixAgent.java
@@ -0,0 +1,18 @@
+package me.ziyframework.agent.palantir.java.format;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * 拦截修改Palantir的Java-Format中xml问题.<br/>
+ * <a href="https://github.com/palantir/palantir-java-format/pull/1478">pull request</a> <br />
+ * created in 2025-11
+ *
+ * @author ziy
+ */
+public class PalantirJavaFormatFixAgent {
+
+    public static void premain(String agentArgs, Instrumentation inst) {
+        System.out.println(">>> [PalantirJavaFormatFixAgent] 已加载，准备修复 Groovy XmlParser 兼容性问题...");
+        inst.addTransformer(new XmlUtilsTransformer());
+    }
+}
---
+++ src/main/java/me/ziyframework/agent/palantir/java/format/XmlUtilsTransformer.java
@@ -0,0 +1,12 @@
+package me.ziyframework.agent.palantir.java.format;
+
+import java.lang.instrument.ClassFileTransformer;
+
+/**
+ * .
+ * created in 2025-11
+ *
+ * @author ziy
+ */
+public class XmlUtilsTransformer implements ClassFileTransformer {
+}

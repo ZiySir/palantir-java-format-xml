ba6cc04cdab156ec880a3c87e9915e7dd0c2b6d6
ziy <1936893077@qq.com>
2025-11-24 19:04:58 +0800

    完成agent开发和打包

--- src/main/java/me/ziyframework/agent/palantir/java/format/XmlUtilsTransformer.java
+++ src/main/java/me/ziyframework/agent/palantir/java/format/XmlUtilsTransformer.java
@@ -1,8 +1,6 @@
 package me.ziyframework.agent.palantir.java.format;
 
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.*;
 import org.objectweb.asm.commons.ClassRemapper;
 import org.objectweb.asm.commons.Remapper;
 import org.objectweb.asm.commons.SimpleRemapper;
@@ -10,6 +8,8 @@ import org.objectweb.asm.commons.SimpleRemapper;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,31 +23,65 @@ public class XmlUtilsTransformer implements ClassFileTransformer {
 
     private static final String TARGET_CLASS_SLASH = "com/palantir/javaformat/gradle/XmlUtils";
 
-    private static final Map<String, String> MAPPING = Map.of("groovy.util.XmlNodePrinter", "groovy.xml.XmlNodePrinter",
-            "groovy.util.XmlParser", "groovy.util.XmlParser");
+    private static final Map<String, String> MAPPING = Map.of(
+            "groovy/util/XmlNodePrinter", "groovy/xml/XmlNodePrinter",
+            "groovy/util/XmlParser", "groovy/xml/XmlParser");
 
     private static final SimpleRemapper REMAPPER = new SimpleRemapper(Opcodes.ASM9, MAPPING);
 
-
-    private static final String TARGET_CLASS_DOT = "com.palantir.javaformat.gradle.XmlUtils";
-
     @Override
-    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) {
         if (className == null || !className.equals(TARGET_CLASS_SLASH)) {
             return null;
         }
+        System.out.println("Loading class: " + className + " from " + loader);
+
 
         System.out.println(">>> [XmlUtilsTransformer] 检测到目标类: " + className);
 
         // 使用 ASM 进行类重映射
         ClassReader classReader = new ClassReader(classfileBuffer);
-        ClassWriter classWriter = new ClassWriter(classReader, 0);
+        ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
         ClassRemapper classRemapper = new ClassRemapper(classWriter, REMAPPER);
 
-        classReader.accept(classRemapper, 0);
+//        classReader.accept(classRemapper, ClassReader.EXPAND_FRAMES);
 
         System.out.println(">>> [XmlUtilsTransformer] 已完成对 " + className + " 的转换");
 
-        return classWriter.toByteArray();
+        StaticInitPrinterAdapter staticInitAdapter = new StaticInitPrinterAdapter(classRemapper);
+
+        classReader.accept(staticInitAdapter, ClassReader.EXPAND_FRAMES);
+
+        byte[] byteArray = classWriter.toByteArray();
+        if (!Arrays.equals(classfileBuffer, byteArray)) {
+            System.out.println(">>> [XmlUtilsTransformer] 字节码已修改");
+        } else {
+            System.out.println(">>> [XmlUtilsTransformer] 字节码未发生变化");
+        }
+        return byteArray;
+    }
+
+    public static class StaticInitPrinterAdapter extends ClassVisitor {
+
+        public StaticInitPrinterAdapter(ClassVisitor cv) {
+            super(Opcodes.ASM9, cv);
+        }
+
+        @Override
+        public void visitEnd() {
+            MethodVisitor mv = cv.visitMethod(Opcodes.ACC_STATIC, "<clinit>", "()V", null, null);
+            mv.visitCode();
+
+            mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+            mv.visitLdcInsn(">>> [XmlUtilsTransformer] Static init block executed!");
+            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println",
+                    "(Ljava/lang/String;)V", false);
+
+            mv.visitInsn(Opcodes.RETURN);
+            mv.visitMaxs(2, 0);
+            mv.visitEnd();
+
+            super.visitEnd();
+        }
     }
 }
